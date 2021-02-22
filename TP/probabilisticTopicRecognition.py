ea71096540cc9916a4445f0f072974123dcd9494
MarcosInfantino <marcosinfantino698@gmail.com>
2021-02-22 19:39:30 -0300

    topic recognition con naive bayes

--- TP/probabilisticTopicRecognition.py
+++ TP/probabilisticTopicRecognition.py
@@ -2,8 +2,9 @@ import docs
 import pickle
 import mainFunctions
 from config import CONFIG
-
+import math
 pathBase = CONFIG["DOCS_DB"] + "/"
+
 
 class PosTaggedWord:
     pos = ""
@@ -34,13 +35,14 @@ class Topic:
         return obj
 
     def addToken(self, token):
-
-        for x in self.tokens:
-            if x == token:
-                return 0
         self.tokens.append(token)
-        return 0
 
+    def count(self):
+        i = 0
+        for x in docsTraining:
+            if x[1].name == self.name:
+                i = i + 1
+        return i
 
 
 topics = []
@@ -49,8 +51,6 @@ domotica = Topic.newTopic("Domótica")
 topics.append(domotica)
 economiaExperiencia = Topic.newTopic("Economía de la experiencia")
 topics.append(economiaExperiencia)
-rifkin = Topic.newTopic("Rifkin")
-topics.append(rifkin)
 sistemasEmergentes = Topic.newTopic("Sistemas emergentes")
 topics.append(sistemasEmergentes)
 marketingEnInternetYNuevaEconomia = Topic.newTopic("Marketing en Internet y Nueva Economía")
@@ -70,44 +70,19 @@ topics.append(sociedadcostoMarginalCero)
 plataformasModelosEBussiness = Topic.newTopic("Plataformas y modelos de e-business")
 topics.append(plataformasModelosEBussiness)
 
-
 docsTraining = [
     ("Domótica_Final.pptx.pptx", domotica),
-    ("Economía de experiencia.pdf", economiaExperiencia),
-    ("K5071 - Matias David Choren - TP N°5 Rifkin.pdf", rifkin),
-    ("K5071 - Matias David Choren - TP N6 Sistemas Emergentes.pdf", sistemasEmergentes),
-    ("K5071.pdf", marketingEnInternetYNuevaEconomia),
-    ("Marketing - TP 0.docx", marketingEnInternetYNuevaEconomia),
-    ("Marketing - TP 2.docx", marketingEnInternetYNuevaEconomia),
-    ("Marketing en Internet y Nueva Economía - TP0.docx", marketingEnInternetYNuevaEconomia),
-    ("MKT 2016 - Alan Szpigiel - TP4 (2).pdf", marketingEnInternetYNuevaEconomia),
-    ("MKT 2016 - Alan Szpigiel - TP4.pdf", marketingEnInternetYNuevaEconomia),
     ("MKTG_TP0  - Definiciones Economia.pdf", definicionesEconomia),
     ("MKTG_TP4  - Suchecki - Comercio Electronico.pdf", comercioElectronicioArgentina),
-    ("Preguntas TP Economía de experiencia - Gabriela Gonzalez.docx", economiaExperiencia),
     ("preguntas TP Wikinomics - Gariglio.doc", wikinomics),
-    ("SCHMID TP N°3 Experience Economy.pdf", marketingEnInternetYNuevaEconomia),
-    ("TP - 4.docx", marketingEnInternetYNuevaEconomia),
     ("TP 0 Gabriela Gonzalez MKTG y NV Economía.doc", marketingEnInternetYNuevaEconomia),
-    ("TP 1 - Larga Cola - Campassi Rodrigo .docx", marketingEnInternetYNuevaEconomia),
     ("TP 1 - Wikinomics (1).pdf", wikinomics),
-    ("TP 1 - Wikinomics.pdf", wikinomics),
-    ("TP 1 Wikinomics.doc", wikinomics),
     ("TP 2 - Franco Zanette.docx", largaCola),
     ("TP 2 - Long Tail.docx", largaCola),
     ("TP 2 - Marketing en Internet y Nueva Economía - Lucas Corbo.docx", marketingEnInternetYNuevaEconomia),
-    ("TP 2 LargaCola - Hernan Noriega .docx", largaCola),
     ("TP 3 - Economía de Experiencia - Andrés Basso (1).docx", economiaExperiencia),
-    ("TP 3 - Economia de la experiencia.docx", economiaExperiencia),
     ("TP 3 - The experience economy - Joseph PINE II y James GILMORE - Juan Cruz Reines.pdf", economiaExperiencia),
-    ("TP 3 (1).doc", economiaExperiencia),
-    ("TP 3 Experience Economy - Hernan Noriega  (1).docx", economiaExperiencia),
-    ("TP 3 Experience Economy.docx", economiaExperiencia),
-    ("TP 3 The experience economy (2).docx", economiaExperiencia),
-    ("TP 3 The experience economy.docx", economiaExperiencia),
-    ("TP 3.docx", economiaExperiencia),
     ("TP 4 - E-commerce - Juan Cruz Reines.pdf", comercioElectronicioArgentina),
-    ("TP 4 - Wikinomía.docx", wikinomics),
     ("TP 4 Difusión y adopción - Hernan Noriega.docx", wikinomics),
     ("TP 4-Franco Zanette (1).docx", difusionAdopcion),
     ("TP 5 - La sociedad de costo marginal cero.docx", sociedadcostoMarginalCero),
@@ -119,24 +94,49 @@ docsTraining = [
     ("TP Adopción TIC - ANTONUCCIO (1).doc", difusionAdopcion),
     ("TP Comercio electronico preguntas Gabriela Gonzalez.doc", comercioElectronicioArgentina),
     ("TP Johnson Sistemas emergentes - ANTONUCCIO.doc", sistemasEmergentes),
-    ("TP La Larga Cola  - ANTONUCCIO.doc", largaCola),
-    ("TP La Larga Cola - Gabriela Gonzalez.doc", largaCola),
     ("TP Larga Cola.docx", largaCola),
-    ("TP N° 1 – WIKINOMICS - Melanie Blejter.pdf", wikinomics),
     ("TP N° 3 – The Experience Economy - Hernán Kotler.docx", economiaExperiencia),
     ("TP N° 4 – Difusión y Adopción TIC - Hernán Kotler.docx", difusionAdopcion),
     ("TP N° 5 – La sociedad de costo marginal cero - Melanie Blejter.pdf", sociedadcostoMarginalCero),
-    ("TP N°1 - Wikinomics (1).pdf", wikinomics),
-    ("TP N°02 (1).pdf", largaCola),
-    ("TP N°2 - La Larga Cola de Chris Anderson corto (1).doc", largaCola),
-    ("TP N°2 Larga Cola.pdf", largaCola)
-
+    ("TP5 - Plataformas.docx", plataformasModelosEBussiness),
+    ("TP 5 - Machine, Platform, Crowd.docx", plataformasModelosEBussiness)
 
 ]
 
 
-def createCacheProbabilisticTopicRecognition():
 
+
+def createVocabulary(tops):
+    list = []
+    for t in tops:
+        for x in t.tokens:
+            list.append(x)
+    return list
+
+
+
+
+
+'''
+for t in topics:
+    count = 0
+    for x in docsTraining:
+        if x[1] == t:
+            count = count + 1
+    print(t.name + "   " + str(count))
+'''
+
+def smoothLikelyhood(word, topic, vocabulary):
+    i = 0
+    for x in topic.tokens:
+        if x.text == word.text:
+            i = i + 1
+
+
+    return (i + 1) / (len(topic.tokens) + len(vocabulary))
+
+
+def createCacheProbabilisticTopicRecognition():
     for x in docsTraining:
 
         docName = x[0]
@@ -155,6 +155,24 @@ def createCacheProbabilisticTopicRecognition():
         pickle.dump(topic, topic_file)
 
 
+
+def containsToken(list, token):
+    bool = False
+    for x in list:
+        if x.text == token.text:
+            bool = True
+
+    return bool
+
+
+def logsumexp(x, y):
+    sum = 0
+    max_value = max(x, y)
+    sum += math.exp(x - max_value)
+    sum += math.exp(y - max_value)
+
+    return math.log(sum) + max_value
+
 def getTopicProbabilistic(doc):
     _topics = []
     files = docs.ls("probabilistic_topic_recognition/")
@@ -172,19 +190,23 @@ def getTopicProbabilistic(doc):
             if bool:
                 tokens_doc.append(tok)
 
-    maxProb = -1
+    maxProb = -10000000
     maxTopic = ""
-
+    vocabulary = createVocabulary(_topics)
     for _topic in _topics:
+        prob = 0
+        prob += math.log(_topic.count()/len(topics)) / math.log(10)
 
-        intersection = len(mainFunctions.intersectionStringTok(tokens_doc, _topic.tokens))
-        union = len(mainFunctions.unionStringTok(tokens_doc, _topic.tokens))
+        for word in tokens_doc:
+            if containsToken(vocabulary, word):
 
 
-        prob = (intersection / union) * 100
+                prob += math.log(smoothLikelyhood(word, _topic, vocabulary)) / math.log(10)
+
 
 
         if prob > maxProb:
+
             maxProb = prob
             maxTopic = _topic
 
